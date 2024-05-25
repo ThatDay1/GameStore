561488f330bf6487bad8b6ea94602cc8353b91e8
ThatDay1 <120332992+ThatDay1@users.noreply.github.com>
2024-05-25 10:07:28 +0300

    using data transfer objects (DTOs)

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,5 +1,31 @@
+using GameStore.Api.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+List<GameDto> games = [
+    new (
+        1,
+        "Street Fighter II",
+        "Fighting",
+        19.99M,
+        new DateOnly(1992, 7, 15)
+    ),
+    new (
+        2,
+        "Final Fantasy XIV",
+        "Roleplaying",
+        59.99M,
+        new DateOnly(2010, 9, 30)
+    ),
+    new (
+        3,
+        "FIFA 23",
+        "Sports",
+        69.99M,
+        new DateOnly(2022, 9, 27)
+    )
+];
 
 app.MapGet("/", () => "Hello World!");
 
--- GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e868d70a31738b8f6d636b0e3b57477368115e11")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
