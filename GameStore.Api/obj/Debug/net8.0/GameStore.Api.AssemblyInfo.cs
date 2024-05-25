a528134d17e51533cea715fef5e88f66898820d6
ThatDay1 <120332992+ThatDay1@users.noreply.github.com>
2024-05-25 19:19:41 +0300

    seeding data

--- GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fb0b67b54cfb174a6c148f65eaa51073c17906a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf76dc80cae8bbe786694df79b41061586686076")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
