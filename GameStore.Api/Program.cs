bf76dc80cae8bbe786694df79b41061586686076
ThatDay1 <120332992+ThatDay1@users.noreply.github.com>
2024-05-25 17:05:53 +0300

    Generating Database

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -3,11 +3,13 @@ using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connString = "Data Source=GameStore.db";
+var connString = builder.Configuration.GetConnectionString("GameStore");
 builder.Services.AddSqlite<GameStoreContext>(connString);
 
 var app = builder.Build();
 
 app.MapGamesEndpoints();
+
+app.MigrateDb();
 
 app.Run();
--- GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caf131894749b02fb21e046c603815962f578695")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fb0b67b54cfb174a6c148f65eaa51073c17906a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
